12926a90711997cf8958d976aa94e0d0e9defa48
João Pedro <jpedrocruz78@gmail.com>
2024-05-07 14:13:27 -0400

    add choose grid button

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 const containerDiv = document.querySelector(".container");
+const promptButton = document.querySelector(".promptButton");
+const INITIAL_CELL_NUMBER = 16;
 
 const cellDivArray = [];
-for (let index = 0; index < 255; index++)
-    {
-        let cell = document.createElement("div");
-        cell.setAttribute("class", "cell");
-        cell.addEventListener("pointerover", () => {
-            cell.style.backgroundColor = "white";
-        })
-        containerDiv.appendChild(cell);
-        cellDivArray.push(cell);
-    }
+function generateGrid(cellNumber){
+    let gridCellNumber = cellNumber * cellNumber
+    for (let index = 0; index < gridCellNumber - 1; index++)
+        {
+            let cell = document.createElement("div");
+            cell.setAttribute("class", "cell");
+            cell.addEventListener("pointerover", () => {
+                cell.style.backgroundColor = "white";
+            })
+            containerDiv.appendChild(cell);
+            cellDivArray.push(cell);
+        }
+}
 
+
+
+promptButton.addEventListener("click", () =>{ 
+    let cellNumber = prompt("How many cells do you want in yout grid?(from 1 to 100)")
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => cell.remove());
+
+});
+
+generateGrid(INITIAL_CELL_NUMBER);
