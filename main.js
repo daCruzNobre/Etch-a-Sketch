205489bd3c043bc130b6e6866cdbcdf8dda02e79
João Pedro <jpedrocruz78@gmail.com>
2024-05-07 13:38:20 -0400

    add hovering effect

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ for (let index = 0; index < 255; index++)
     {
         let cell = document.createElement("div");
         cell.setAttribute("class", "cell");
+        cell.addEventListener("pointerover", () => {
+            cell.style.backgroundColor = "white";
+        })
         containerDiv.appendChild(cell);
         cellDivArray.push(cell);
     }
+
