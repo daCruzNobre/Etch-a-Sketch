35248667cdbd2c9161a70d70670a967129b4ae6e
João Pedro <jpedrocruz78@gmail.com>
2024-05-08 10:18:58 -0400

    finish random and opacity features

--- main.js
+++ main.js
@@ -4,17 +4,28 @@ const resetButton = document.querySelector(".resetButton");
 const INITIAL_CELL_NUMBER = 16;
 
 const cellDivArray = [];
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
 function generateGrid(cellNumber){
     let gridCellNumber = cellNumber * cellNumber
     for (let index = 0; index < gridCellNumber; index++)
         {
+            let rgbOne = getRndInteger(0,255);
+            let rgbTwo = getRndInteger(0,255);
+            let rgbThree = getRndInteger(0,255);            
+            let opacityLevel = Math.random();
+            let backgroundColor1 = "rgb(" + rgbOne + " " + rgbTwo + " " + rgbThree + " " + "/" + " " + opacityLevel+ ")";
             let cell = document.createElement("div");
             cell.setAttribute("class", "cell");
             cell.addEventListener("pointerover", () => {
-                cell.style.backgroundColor = "white";
-            })
+                cell.style.backgroundColor = backgroundColor1;
+            })            
             containerDiv.appendChild(cell);
             cellDivArray.push(cell);
+           
         }
 }
 
